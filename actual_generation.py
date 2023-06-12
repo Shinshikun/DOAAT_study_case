83673312c25591edb7759d634bbdadb388abd822
Shinshikun <thomasdelaisse@gmail.com>
2023-06-13 00:04:42 +0200

    Implémentation de la moyenne par heure

--- actual_generation.py
+++ actual_generation.py
@@ -1,6 +1,8 @@
 import requests
 import datetime
 import urllib
+import pandas as pd
+import dateutil.parser 
 from typing import Optional
 
 import config
@@ -13,17 +15,22 @@ class ActualGeneration():
     def __init__(self):
         self.access_token, self.token_type = self.get_token()
 
-    def get_per_unit(self, start_date: Optional[datetime.datetime] = None, end_date: Optional[datetime.datetime] = None) -> dict:
+    def get_per_unit(self, start_date: Optional[datetime.datetime] = None, end_date: Optional[datetime.datetime] = None, sandbox=True) -> dict:
+
+        if sandbox:
+            url = "https://digital.iservices.rte-france.com/open_api/actual_generation/v1/sandbox/actual_generations_per_unit"
+        else:
+            url = "https://digital.iservices.rte-france.com/open_api/actual_generation/v1/actual_generations_per_unit"
+
 
         if start_date is None or end_date is None:
-            response = requests.get('https://digital.iservices.rte-france.com/open_api/actual_generation/v1/actual_generations_per_unit',
-                                    headers={'Authorization': f'{self.access_token} {self.token_type} '}
-                                )
+            response = requests.get(url,
+                                    headers={'Authorization': f'{self.access_token} {self.token_type} '})
         else:
             start = start_date.strftime("%Y-%m-%dT%H:%M:%S+02:00")
             end = end_date.strftime("%Y-%m-%dT%H:%M:%S+02:00")
             param = urllib.parse.urlencode({"start_date": start, "end_date": end}).replace("%3A", ":")
-            response = requests.get('https://digital.iservices.rte-france.com/open_api/actual_generation/v1/actual_generations_per_unit',
+            response = requests.get(url,
                                     headers={'Authorization': f'{self.access_token} {self.token_type} '},
                                     params=param
                                 )
@@ -34,10 +41,25 @@ class ActualGeneration():
             return response.status_code
 
     
-    def get_mean_hour_by_hour(self):
-        
-    
-    
+    def get_mean_hour_by_hour(self, start_date: Optional[datetime.datetime] = None, end_date: Optional[datetime.datetime] = None, sandbox=True):
+        data = self.get_per_unit(start_date, end_date, sandbox=sandbox)
+        value_per_hour = {}
+        if type(data) != dict:
+            print("Erreur lors de l'appel à l'API")
+            return
+        data = data.get("actual_generations_per_unit")
+
+        for entry in data:
+            for values in entry["values"]:
+                date = dateutil.parser.parse(values["start_date"])
+                if date in value_per_hour:
+                    value_per_hour[date] += values["value"]
+                else:
+                    value_per_hour[date] = values["value"]
+
+        d = pd.Series(value_per_hour).resample("H").mean()
+        print(d)
+
 
     def get_token(self):
 
--- main.py
+++ main.py
@@ -7,8 +7,7 @@ from actual_generation import *
 
 if __name__ == '__main__':
     start = datetime.datetime(2022, 12, 1, 0, 0)
-    end = datetime.datetime(2022, 12, 7, 0, 0)
+    end = datetime.datetime(2022, 12, 4, 0, 0)
 
     api = ActualGeneration()
-    actual_gen_per_unit = DataFrame(api.get_per_unit(start, end))
-    print(actual_gen_per_unit.head())
+    api.get_mean_hour_by_hour(start_date=start, end_date=end, sandbox=False)
